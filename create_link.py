ef27328fd1b87323a03eb5f742d940fdf85f69fb
PavanKumar-sudo <pavanvinjamuri212@gmail.com>
2025-06-12 18:14:18 -0400

    sdwq

--- create_link.py
+++ create_link.py
@@ -1,21 +1,20 @@
-import json
 import os
 import boto3
+import json
 import uuid
-
 
 def lambda_handler(event, context):
     table_name = os.environ['TABLE_NAME']
-    table = boto3.resource('dynamodb').Table(table_name)
+    region = os.environ.get('AWS_REGION', 'us-east-1')  # fallback if not set
+
+    table = boto3.resource('dynamodb', region_name=region).Table(table_name)
 
     body = json.loads(event['body'])
     code = str(uuid.uuid4())[:6]
     long_url = body['url']
     table.put_item(Item={'code': code, 'url': long_url})
 
-    short_url = f"https://{event['headers']['host']}/{code}"
-
     return {
         "statusCode": 200,
-        "body": json.dumps({"short_url": short_url})
+        "body": json.dumps({"short_url": f"https://{event['headers']['host']}/{code}"})
     }
--- redirect.py
+++ redirect.py
@@ -1,9 +1,9 @@
 import os
 import boto3
 
-
 def lambda_handler(event, context):
-    table = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
+    region = os.environ.get('AWS_REGION', 'us-east-1')
+    table = boto3.resource('dynamodb', region_name=region).Table(os.environ['TABLE_NAME'])
 
     code = event['pathParameters']['code']
     response = table.get_item(Key={'code': code})
